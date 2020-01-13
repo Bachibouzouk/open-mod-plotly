2842cf211744dbcf779d991b68d8f3ef685aa331
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-01-13 16:43:50 +0100

    Change mode to type

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ demo_app.layout = html.Div(
             id='example-graph',
             figure={
                 'data': [
-                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'mode': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'mode': 'bar', 'name': u'Montréal'},
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
                 ],
                 'layout': {
                     'title': 'Dash Data Visualization'
